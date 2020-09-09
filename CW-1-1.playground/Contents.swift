8ac826725b10bdb0ed323412c543b242c0576fac
Bassam-AlBassam <bassamy3q8@gmail.com>
2020-09-09 20:47:45 +0300

    done

---
+++ CW-1-1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// first let
+let name = "bassam albassam"
+// firt var
+var hobby1 = "football"
+var hobby2 = "table tennis"
+var age = 17
+var height  = 172
+print(name ,hobby1 ,hobby2 ,age ,height )
+print("My name is \(name) my age \(age)")
+let friendName = "khaled bourisly"
+var friendHeight = "174"
+var friendAge = 16
+var friendHobby1 = "table tennis"
+var friendHobby2 = "swimming"
+age - friendAge
+var mutualHobbies = hobby1 == friendHobby1 || hobby2 == friendHobby2
+print(mutualHobbies)
+
